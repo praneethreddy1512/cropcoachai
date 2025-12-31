da0d2536d51a3a71608dd86b7a7633ac3773248a
Praneeth <thumu.praneethreddy@gmail.com>
2025-12-31 11:50:22 +0530

    sloved minor issue

--- server/vite.ts
+++ server/vite.ts
@@ -51,7 +51,7 @@ export async function setupVite(app: Express, server: Server) {
     if (req.originalUrl.startsWith("/api")) {
       return next();
     }
-    
+
     const url = req.originalUrl;
 
     try {
@@ -86,7 +86,16 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
+  app.use(express.static(distPath, {
+    maxAge: "1d",
+    setHeaders: (res, path) => {
+      if (path.endsWith(".js")) {
+        res.setHeader("Content-Type", "application/javascript");
+      } else if (path.endsWith(".css")) {
+        res.setHeader("Content-Type", "text/css");
+      }
+    },
+  }));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
